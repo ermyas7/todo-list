4b7d94972c843386ff9977db4fe8807c1c097e46
ermyas7 <ermyas.fekadu@gmail.com>
2019-03-12 19:42:28 +0530

    Add todos component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Todos from './components/Todos';
 class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'finish react app',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'finish vue app',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'set up deployment',
+        completed: false
+      },
+    ]
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
---
+++ src/components/Todos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Todos extends Component {
+  render() {
+    return this.props.todos.map((todo) => (
+        <h3>{todo.title} </h3>
+    ))
+  }
+}
+
+export default Todos
